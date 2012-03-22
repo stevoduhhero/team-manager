e2d43521721a672a31d06fbd8c2e4667785ed43b
StevoDuhHero <pgts2010@yahoo.com>
2012-03-21 17:12:32 -0700

    Initial commit

--- includes.php
+++ includes.php
@@ -4,6 +4,8 @@
   include 'functions.php';
   
   $foldername = "team-manager";
+  $timeout = 10;
+  
   $ip = $_SERVER['REMOTE_ADDR'];
   date_default_timezone_set('America/Los_Angeles');
   $time = time();
@@ -16,4 +18,12 @@
   }
   $username_clean = clean($username);
   $user_id = sqlsingle("SELECT id FROM users WHERE username='$username_clean'");
+  
+  //kick inactive
+  $cutoff = $time-(60*$timeout);
+  $sql = "UPDATE users SET online='0' WHERE last_activity<'$cutoff'";
+  mysql_query($sql) or die(mysql_error());
+  //update activity
+  $sql = "UPDATE users SET online='1' AND last_activity='$time' WHERE id='$user_id'";
+  mysql_query($sql) or die(mysql_error());
 ?>
