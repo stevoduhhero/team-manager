7ec5be9fae93874fb7f560f29e11bf6c6db5feb7
StevoDuhHero <pgts2010@yahoo.com>
2012-03-25 16:23:00 -0700

    Initial commit

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
   include 'includes.php';
   include 'header.html';
 ?>
-<div class="box"><h1>Sorry the database was accidentally cleared. If you already had an account you're going to have to register again... :(</h1></div>
 <?php
 	//latest teams
 	echo '<h2>Latest Teams:</h2>';
